4e922656a0b0978d76f2f9ca75f612eba769503f
Rithik <rithikruttala@gmail.com>
2026-05-10 12:49:39 +0530

    Removed hamburger menu and cleaned navbar, kept animations working

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 const button = document.getElementById("theme-toggle");
 
-/* check saved theme when page loads */
 if(localStorage.getItem("theme") === "light"){
-
     document.body.classList.add("light-mode");
-
     button.textContent = "☀";
-
 }
 
-/* button click */
 button.addEventListener("click", function(){
 
     document.body.classList.toggle("light-mode");
 
-    /* if light mode is active */
     if(document.body.classList.contains("light-mode")){
-
         button.textContent = "☀";
-
         localStorage.setItem("theme", "light");
-
     }
     else{
-
         button.textContent = "🌙";
-
         localStorage.setItem("theme", "dark");
-
     }
 
 });
